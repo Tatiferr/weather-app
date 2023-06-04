a5ce1129040398e503e5a7cf7932c741c73035df
Tatiane <taticfkoh@gmail.com>
2023-06-04 19:08:07 -0300

    weather-app (Curso Angular)

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,18 @@
+import { faDroplet, faTemperatureHigh, faTemperatureLow, faWind } from '@fortawesome/free-solid-svg-icons';
+import { WeatherDatas } from './../../../../models/interfaces/WeatherDatas';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent {
+  @Input() weatherDatasInput!: WeatherDatas;
+//DADOS DA PREVISÃO DO TEMPO RECEBIDO PELO COMPONENTE PAI
+
+  minTemperatureIcon = faTemperatureLow;
+  maxTemperatureIcon = faTemperatureHigh;
+  humidityIcon = faDroplet;
+  windIcon = faWind;
+}
